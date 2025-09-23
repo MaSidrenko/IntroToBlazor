2d14885156b5ba8fe45ae3858c16c4e7a460a93a
MaSidrenko <nexon863@gmail.com>
2025-09-23 11:38:39 +0300

    Directions CRUD

---
+++ Academy/Program.cs
@@ -0,0 +1,39 @@
+using Academy.Components;
+using Academy.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components.QuickGrid.EntityFrameworkAdapter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<Pv319ImportContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Pv319ImportContext")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -1,14 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 
 namespace Movies;
 
 public class Movie
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string? Title { get; set; }
+    [RangeAttribute(typeof(DateOnly), "14-10-1888", "31-12-2032")]
     public DateOnly ReleaseDate { get; set; }
+    [Required]
+    [StringLength(30)]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z()\s\s-]*$")]
     public string? Genre { get; set; }
+    [Range(0, 200)]
     [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
